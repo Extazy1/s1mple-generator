d277f633a8f4d36ff8b4529b6413c10f349a1e12
Extazy1 <2056345020@qq.com>
2024-09-11 15:43:09 +0800

    fix:add GenerateTemplate

--- s1mple-generator-maker/src/main/java/com/extazy/Main.java
+++ s1mple-generator-maker/src/main/java/com/extazy/Main.java
@@ -1,15 +1,18 @@
 package com.extazy;
 
+import com.extazy.maker.generator.Main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) {
-//        args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-//        args = new String[]{"list"};
-//        CommandExecutor commandExecutor = new CommandExecutor();
-//        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
     }
 }
+
 
