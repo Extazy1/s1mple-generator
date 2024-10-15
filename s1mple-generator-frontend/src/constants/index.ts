d1e5b609a788fdfc74714afcbaa008f8ea79025c
Extazy1 <2056345020@qq.com>
2024-10-15 21:18:08 +0800

    init:basic web-frontend template

---
+++ s1mple-generator-frontend/src/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 本地后端地址
+ * localhost和127.0.0.1不能等价在这里
+ */
+export const BACKEND_HOST_LOCAL = 'http://localhost:8121';
+
+/**
+ * 线上后端地址
+ */
+export const BACKEND_HOST_PROD = 'https://extazy.s1mple';
